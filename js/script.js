4653011d27189de7e4046a5d903b0522c16c7587
Uros Tomic <urostomic94@gmail.com>
2021-07-09 21:58:53 +0200

    sixth commit

--- js/script.js
+++ js/script.js
@@ -1,52 +1,67 @@
 let submitBtn = document.getElementById("submit-btn");
-let formInputs = document.querySelectorAll(".form__input");
-
-
-//let error = input.nextElementSibling;
-//error.innerHTML = `${input.placeholder}` + " " + `${errorMessage}`;
 
 submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
-    let errors = validateForm();
-    if (errors.length === 0) {
+    let foundErrors = validateForm();
+    if (foundErrors.length === 0) {
         document.getElementById("free-trial-form").submit();
     }
     else {
-        displayError(errors)
+        displayErrorMessage(foundErrors);
+        removeErrorMessage(foundErrors);
     }
 });
 
 function validateForm() {
     let failures = [];
-    formInputs.forEach(formInput => {
-        if (formInput.value === "") {
-            failures.push({ input: `${formInput.id}`, errorMessage: `${formInput.placeholder}` + " " + "cannot be empty" })
+    let firstName = document.getElementById("first-name");
+    let lastName = document.getElementById("last-name");
+    let email = document.getElementById("email-address");
+    let password = document.getElementById("password");
+    if (firstName.value === "") {
+        failures.push({ input: `${firstName.id}`, errorMessage: "First Name cannot be empty" })
+    }
+    if (lastName.value === "") {
+        failures.push({ input: `${lastName.id}`, errorMessage: "Last Name cannot be empty" })
+    }
+    if (password.value === "") {
+        failures.push({ input: `${password.id}`, errorMessage: "Password cannot be empty" })
+    }
+    if (email.value == "") {
+        failures.push({ input: `${email.id}`, errorMessage: "Email cannot be empty" })
+    }
+    else {
+        let emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+        if (!emailFormat.test(email.value)) {
+            failures.push({ input: `${email.id}`, errorMessage: "Looks like this is not an email" });
         }
-        if (formInput.type === "email" && !formInput.value == "") {
-            let emailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
-            if (!emailFormat.test(formInput.value)) {
-                failures.push({ input: `${formInput.id}`, errorMessage: "Looks like this is not an email" });
-            }
-        }
-    });
+    }
     return failures;
 }
 
-function displayError(errors) {
-    errors.forEach(error => {
-        let formInput = document.getElementById(error.input)
-        let formErrorMessage = formInput.nextElementSibling;
-        formErrorMessage.innerHTML = error.errorMessage;
-        formErrorMessage.style.opacity = 1;
-        formInput.classList.add('form__input--error');
+function displayErrorMessage(foundErrors) {
+    foundErrors.forEach(foundError => {
+        let targetInput = document.getElementById(foundError.input)
+        let targetErrorField = targetInput.nextElementSibling;
+        targetErrorField.innerHTML = foundError.errorMessage;
+        targetErrorField.classList.add('trial-claim__form-error-message--active');
+        targetInput.classList.add('trial-claim__form-input--error');
+        targetInput.placeholder = "";
     });
 }
 
-function removeError() {
-    formInputs.forEach(formInput => {
-        formInput.addEventListener('keydown', () => {
-            formInput.classList.remove('form__input--error');
+function removeErrorMessage(foundErrors) {
+    foundErrors.forEach(foundError => {
+        let targetInput = document.getElementById(foundError.input);
+        targetInput.addEventListener("keydown", () => {
+            let targetErrorField = targetInput.nextElementSibling;
+            targetErrorField.innerHTML = "";
+            targetErrorField.classList.remove('trial-claim__form-error-message--active');
+            targetInput.classList.remove('trial-claim__form-input--error');
+            function capitalize(str) {
+                return str.charAt(0).toUpperCase() + str.slice(1);
+            }
+            targetInput.placeholder = `${targetInput.id.split("-").map(capitalize).join(' ').toString()}`;
         })
     });
 }
-removeError();
